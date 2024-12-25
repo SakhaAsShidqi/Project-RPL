5415dec2faab9a0c702387d124cbe31bf61746a8
asayahandatgr <asayahandategar10@gmail.com>
2024-12-25 22:08:29 +0700

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,18 @@
   <meta name="author" content="" />
 
   <title>Siap Bantu</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
   <!-- slider stylesheet -->
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
   <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css')} }" />
-  <!-- font awesome style -->s
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css')}}" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <!-- font awesome style -->
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
 
   <!-- Custom styles for this template -->
-  <link href="{{ asset('css/style.css')}}" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet" />
   <!-- responsive style -->
-  <link href="{{ asset('css/responsive.css')}}" rel="stylesheet" />
+  <link href="css/responsive.css" rel="stylesheet" />
 
 </head>
 
@@ -520,7 +518,6 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap"></script>
   <!-- End Google Map -->
 
-<script>src="/public/siap bantu/siap bantu/js/bootstrap.js"</script>
 
 </body>
 
